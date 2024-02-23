46b14765f5b024c4b51f0198bab41608e74f4d7d
Gabriel Guzman <gguzman@boss.tech>
2024-02-22 18:20:27 -0600

    Gato

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ def resta(num_1: int, num_2: int) -> int:
     return num_1 - num_2
 
 
+def cat_dogs():
+    jodiendo_a_naps = 0
+    return
+
+
 if __name__ == '__main__':
     print(suma(num_1=2, num_2=4))
     print(resta(num_1=2, num_2=5))
